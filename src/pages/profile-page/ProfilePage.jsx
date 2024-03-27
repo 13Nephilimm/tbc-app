fbd00ad09005608f2f6f4df728413738aa5d07cf
13Nephilimm <jekokharabadze@gmail.com>
2024-03-27 11:50:50 +0400

    5

--- src/pages/profile-page/ProfilePage.jsx
+++ src/pages/profile-page/ProfilePage.jsx
@@ -6,9 +6,37 @@ import { CgProfile } from "react-icons/cg";
 const ProfilePage = () => {
   return (
     <Layout>
+      <h1 className="profile-heading">My Profile</h1>
       <div className="profile-container">
-        <CgProfile color="#fff" width={256} />
-        <div className="profile-info"></div>
+        <CgProfile size={256} color="#fff" />
+        <div className="profile-info">
+          <h3 className="profile-name">
+            <b>Name: </b> Jax
+          </h3>
+          <h3 className="profile-lastname">
+            <b>Lastname: </b> Teller
+          </h3>
+          <h3 className="profile-email">
+            <b>Email: </b> jaxteller@gmail.com
+          </h3>
+          <input
+            className="profile-password"
+            type="password"
+            name="password"
+            placeholder="New Password"
+            required
+          />
+          <input
+            className="profile-password"
+            type="password"
+            name="password"
+            placeholder="Confirm New Password"
+            required
+          />
+          <button type="submit" className="secondary-btn btn-password-save">
+            Save
+          </button>
+        </div>
       </div>
     </Layout>
   );
