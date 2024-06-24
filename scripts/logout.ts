e2f1de408d51aece20feb606ad91f39aff2c6393
13Nephilimm <jekokharabadze@gmail.com>
2024-06-24 04:15:24 -0700

    deployment

--- scripts/logout.ts
+++ scripts/logout.ts
@@ -1,5 +1,5 @@
 export const handleLogout = async (): Promise<void> => {
-  await fetch(`${process.env}/api/logout`, {
+  await fetch(`${process.env.BASE_URL}/api/logout`, {
     method: "POST",
   });
 };
